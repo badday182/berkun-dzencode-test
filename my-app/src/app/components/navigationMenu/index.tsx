8bb0bcf6fbdd6f1d8e7ae391546109339ea8ff3b
Valerii Berkun <badday182@gmail.com>
2025-03-17 14:01:36 +0200

    Add clsx dependency and enhance navigation menu with active link styling

--- my-app/src/app/components/navigationMenu/index.tsx
+++ my-app/src/app/components/navigationMenu/index.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const NavigationMenu = () => {
+  const pathname = usePathname();
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link" href="products">
+            <Link
+              className={clsx("nav-link", { active: pathname === "/products" })}
+              href="products"
+            >
               Products
             </Link>
           </li>
           <li>
-            <Link className="nav-link" href="orders">
+            <Link
+              className={clsx("nav-link", { active: pathname === "/orders" })}
+              href="orders"
+            >
               Orders
             </Link>
           </li>
