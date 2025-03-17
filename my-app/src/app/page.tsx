85ef63f48711e395fcd30cef51a8cbad4f8fcea1
Valerii Berkun <badday182@gmail.com>
2025-03-17 16:42:47 +0200

    Add socket.io and socket.io-client dependencies; clean up link formatting in Home component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,22 +1,15 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>    
+      <main className={styles.main}>
         <div className={styles.ctas}>
-          <Link
-            className={styles.primary}
-            href="products"
-          >           
+          <Link className={styles.primary} href="products">
             Products
           </Link>
-          <Link
-            href="orders"
-            className={styles.secondary}
-          >
+          <Link href="orders" className={styles.secondary}>
             Orders
           </Link>
         </div>
