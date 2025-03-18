ff8884a748e8dc61279189fe5a63b477a1de95d7
Valerii Berkun <badday182@gmail.com>
2025-03-18 11:07:00 +0200

    Add multiple orders and products to the Orders component

--- my-app/src/app/orders/page.tsx
+++ my-app/src/app/orders/page.tsx
@@ -46,8 +46,28 @@ const Orders = () => {
             title: "Order 1",
             date: "2017-06-29 12:09:33",
             description: "desc",
+            get products() {
+              return products;
+            },
           },
-          // Other orders...
+          {
+            id: 2,
+            title: "Order 2",
+            date: "2018-05-21 12:09:33",
+            description: "desc",
+            get products() {
+              return products;
+            },
+          },
+          {
+            id: 3,
+            title: "Order 3",
+            date: "2019-04-10 12:09:33",
+            description: "desc",
+            get products() {
+              return products;
+            },
+          },
         ];
 
         const products = [
@@ -106,6 +126,120 @@ const Orders = () => {
               { value: 2600, symbol: "UAH", isDefault: 1 },
             ],
             order: 1,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 4,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 45, symbol: "USD", isDefault: 0 },
+              { value: 1800, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 2,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 5,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 5, symbol: "USD", isDefault: 0 },
+              { value: 40, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 2,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 6,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 45, symbol: "USD", isDefault: 0 },
+              { value: 555, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 2,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 7,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 3, symbol: "USD", isDefault: 0 },
+              { value: 150, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 3,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 8,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 2, symbol: "USD", isDefault: 0 },
+              { value: 100, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 3,
+            date: "2017-06-29 12:09:33",
+          },
+          {
+            id: 9,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: "pathToFile.jpg",
+            title: "Product 1",
+            type: "Monitors",
+            specification: "Specification 1",
+            guarantee: {
+              start: "2017-06-29 12:09:33",
+              end: "2017-06-29 12:09:33",
+            },
+            price: [
+              { value: 1, symbol: "USD", isDefault: 0 },
+              { value: 50, symbol: "UAH", isDefault: 1 },
+            ],
+            order: 3,
             date: "2017-06-29 12:09:33",
           },
         ];
