929b96d93b138e2a21857483938c69eb2b989701
Valerii Berkun <badday182@gmail.com>
2025-03-19 23:26:00 +0200

    add readme

--- my-app/src/components/productsCard/index.tsx
+++ my-app/src/components/productsCard/index.tsx
@@ -59,61 +59,59 @@ const ProductCard: React.FC<{ product?: Product }> = ({
   return (
     <>
       <div className={`card shadow-sm flex-grow-1 ${styles.card}`}>
-        <div className="card-body">
-          <div className="d-flex justify-content-between align-items-center gap-5">
-            <div
-              className={`d-flex flex-grow-1 justify-content-between align-items-center gap-5`}
-            >
-              <div className="d-flex justify-content-between align-items-center gap-3 flex-grow-1">
-                <h5 className="card-title flex-grow-1 m-0">{product.title}</h5>
-                <span
-                  className={`badge bg-secondary flex-shrink-0 ${styles.wordWrap}`}
-                >
-                  {product.type}
-                </span>
-              </div>
-
-              <div className="d-flex flex-column align-items-center flex-shrink-0">
-                <div>
-                  <span className="text-muted">с: </span>
-                  <span className="text-muted">{guaranteeStartDate}</span>
-                </div>
-                <div>
-                  <span className="text-muted">по: </span>
-                  <span className="text-muted fs-6">{guaranteeEndDate}</span>
-                </div>
-              </div>
-
-              <div className="d-flex flex-column flex-shrink-0">
-                <div className="d-flex flex-column">
-                  <div className="text-muted fs-6">{usdPrice?.value} USD</div>
-                  <div className="text-muted fw-medium fs-5">
-                    {uahPrice?.value} UAH
-                  </div>
-                </div>
-              </div>
-
-              <div
-                className={clsx(
-                  "text-muted d-flex flex-column flex-shrink-0",
-                  styles.wordWrap,
-                  styles.smallFontSize,
-                  {
-                    "d-none": isOpenAsideContainer && pathname === "/orders",
-                  }
-                )}
+        <div className="card-body d-flex justify-content-between align-items-center gap-5">
+          <div
+            className={`d-flex flex-grow-1 justify-content-between align-items-center gap-5`}
+          >
+            <div className="d-flex justify-content-between align-items-center gap-3 flex-grow-1">
+              <h5 className="card-title flex-grow-1 m-0">{product.title}</h5>
+              <span
+                className={`badge bg-secondary flex-shrink-0 ${styles.wordWrap}`}
               >
-                {orderTitle}
+                {product.type}
+              </span>
+            </div>
+
+            <div className="d-flex flex-column align-items-center flex-shrink-0">
+              <div>
+                <span className="text-muted">с: </span>
+                <span className="text-muted">{guaranteeStartDate}</span>
+              </div>
+              <div>
+                <span className="text-muted">по: </span>
+                <span className="text-muted fs-6">{guaranteeEndDate}</span>
               </div>
             </div>
 
-            <button className="btn btn-sm">
-              <i
-                className={`bi bi-trash ${styles.icon}`}
-                onClick={handleOpenModal}
-              ></i>
-            </button>
+            <div className="d-flex flex-column flex-shrink-0">
+              <div className="d-flex flex-column">
+                <div className="text-muted fs-6">{usdPrice?.value} USD</div>
+                <div className="text-muted fw-medium fs-5">
+                  {uahPrice?.value} UAH
+                </div>
+              </div>
+            </div>
+
+            <div
+              className={clsx(
+                "text-muted d-flex flex-column flex-shrink-0",
+                styles.wordWrap,
+                styles.smallFontSize,
+                {
+                  "d-none": isOpenAsideContainer && pathname === "/orders",
+                }
+              )}
+            >
+              {orderTitle}
+            </div>
           </div>
+
+          <button className="btn btn-sm">
+            <i
+              className={`bi bi-trash ${styles.icon}`}
+              onClick={handleOpenModal}
+            ></i>
+          </button>
         </div>
       </div>
       {showModal && (
